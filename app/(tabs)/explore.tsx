501a54939cfc0f151ddf1068724e69bb656486ff
unknown <cyberrashad@gmail.com>
2024-05-30 07:56:56 +0700

    menambahkan hal yg seharusnya ga terlallu berguna tapi buat keren2 an aja (gabwut si)

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const explore = () => {
   return (
-    <View>
-      <Text>explore</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Sebenarnya Halaman ini tadinya tidak niatan untuk di buat, tetapi karena sang Developer gabut jadinya sengaja kebuat dengan sendirinya</Text>
+      <Text style={styles.paragraph}>Singat saja ini lah Kami :</Text>
+      <Text style={styles.paragraph}>Muhammad Rashad Fawzy AL - Baariq | Single Page Function Improvement</Text>
+      <Text style={styles.paragraph}>Muhammad Raditya Javana | Logic Function Maker</Text>
     </View>
   )
 }
 
-export default explore
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  paragraph: {
+    fontSize: 24,
+    fontWeight: "400",
+    marginVertical: 12,
+  }
+});
+
+export default explore;
+
